2ea58d0272b03abcd634174aae3605b44b0db83e
jahnavi2k <jahnavi2k@gmail.com>
2023-03-07 21:09:14 +0530

    changes

--- pages/about.py
+++ pages/about.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from st_functions import st_button, load_css
+from ABOUT.st_functions import st_button
 
 st.title(':blue[OUR TEAM]')
 
@@ -11,27 +11,27 @@ col1, col2, col3, col4 = st.columns(4)
 icon_size = 20
 
 with col1:
-    st.image("Images/poojadinew.png")
+    st.image("ABOUT\Images\poojadinew.png")
     st.caption("Pooja Gera")
     st_button('linkedin', 'https://www.linkedin.com/in/pooja-gera/',
               'LinkedIn', icon_size)
 
 
 with col2:
-    st.image("Images/NishthaDinew.png")
+    st.image("ABOUT\Images\ShreyaGanjoo.jpeg")
     st.caption("Nishtha Goyal")
     st_button('linkedin', 'https://www.linkedin.com/in/nishtha0801/',
               'LinkedIn', icon_size)
 
 
 with col3:
-    st.image("Images/GaurishaDinew.png")
+    st.image("ABOUT\Images\GaurishaDinew.png")
     st.caption("Gaurisha R Srivastava")
     st_button('linkedin', 'https://www.linkedin.com/in/gaurisha-r-srivastava/',
               'LinkedIn', icon_size)
 
 with col4:
-    st.image("Images/AbhigyaDi.png")
+    st.image("ABOUT\Images\AbhigyaDi.png")
     st.caption("Abhigya Verma")
     st_button('linkedin', 'https://www.linkedin.com/in/abhigya02/',
               'LinkedIn', icon_size)
@@ -48,35 +48,35 @@ st.header('*:black[MEMBERS]*')
 col5, col6, col7 = st.columns(3)
 
 with col5:
-    st.image("Images/ShreyaGanjoo.jpeg")
+    st.image("ABOUT\Images\ShreyaGanjoo.jpeg")
     st.caption("Shreya Ganjoo")
     st_button('linkedin', 'https://www.linkedin.com/in/shreya-ganjoo-30563822b/',
               'LinkedIn', icon_size)
 
-    st.image("Images/AnushkaAggarwal.png")
+    st.image("ABOUT\Images\AnushkaAggarwal.png")
     st.caption("Anushka Agarwal")
     st_button('linkedin', 'https://www.linkedin.com/in/anushka-aggarwal-26963b228/',
               'LinkedIn', icon_size)
 
 
 with col6:
-    st.image("Images/KashikaDi.jpg")
+    st.image("ABOUT\Images\KashikaDi.jpg")
     st.caption("Kashika Jain")
     st_button('linkedin', 'https://www.linkedin.com/in/kashika18/',
               'LinkedIn', icon_size)
 
-    st.image("Images/KhushiJainDi.jpg")
+    st.image("ABOUT\Images\KhushiJainDi.jpg")
     st.caption("Khushi Jain")
     st_button('linkedin', 'https://www.linkedin.com/in/khushi-jain-bb3bb9201/',
               'LinkedIn', icon_size)
 
 with col7:
-    st.image("Images/JahnaviDi.jpg")
+    st.image("ABOUT\Images\JahnaviDi.jpg")
     st.caption("Jahnavi Malhotra")
     st_button('linkedin', 'https://www.linkedin.com/in/jahnavi2k/',
               'LinkedIn', icon_size)
 
-    st.image("Images/KhushiSinhaDi.jpeg")
+    st.image("ABOUT\Images\KhushiSinhaDi.jpeg")
     st.caption("Khushi Sinha")
     st_button('linkedin', 'https://www.linkedin.com/in/khushi-sinha-91731b202/',
               'LinkedIn', icon_size)
