7557fddf452cc6b1fb70e65001aac80b5647db44
jahnavi2k <jahnavi2k@gmail.com>
2023-03-08 17:25:27 +0530

    changes

--- pages/entryNotRight.py
+++ pages/entryNotRight.py
@@ -1,17 +1,18 @@
-import final_car_plate_number_recognition
 import streamlit as st
+import final_car_plate_number_recognition
 from AB.nav import *
+from pages.MakeEntry import *
 from google.cloud import firestore
 
-number_plate = final_car_plate_number_recognition.text
-st.write(number_plate)
+# st.text_input('daalo bhai number', value="initial_value")
+name = st.text_input(
+    "Please input correct license plate number", value=number_plate)
+# name = st.text_input("Please input correct license plate number")
 
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
 db = firestore.Client.from_service_account_json("firestore-key.json")
-
-# for entering data: number will be text received from model-
 
 
 def ent():
@@ -19,19 +20,14 @@ def ent():
         "car" + str(st.session_state.count))
     st.session_state.count += 1
     doc_ref.set({
-        "Number": number_plate,
+        "Number": name,
         "InTime": firestore.SERVER_TIMESTAMP
     })
 
 
-cap_button = st.button("Enter")
+cap_button1 = st.button("Enter now")
 
-if cap_button:
+if cap_button1:
     ent()
     st.text("Captured Successfully")
     nav_page("Table")
-
-not_correct = st.button('Number is not correct')
-
-if not_correct:
-    nav_page('entryNotRight')
