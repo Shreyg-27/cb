df7a4cfa98fad1d6bfeb3498e177c5d16b2bd719
Khushis01 <sinhak2110@gmail.com>
2023-03-09 00:06:16 +0530

    Branding of the website

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pyrebase
 import streamlit as st
 from datetime import datetime
 from AB.nav import *
+import streamlit as st
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
 
 
 st.title("Project Name")
@@ -70,7 +74,7 @@ if choice == 'Login':
         st.write(
             '<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
         st.title('Welcome')
-        
+
         #st.balloons()
         b1 = st.button("Login")
         if b1:
@@ -79,3 +83,77 @@ if choice == 'Login':
         # bio = st.radio('Jump to', ['Home'])
         # if bio == 'Home':
             # col1, col2 = st.columns(2)
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+def layout(*args):
+
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;}
+     .stApp { bottom: 100px; }
+    </style>
+    """
+
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="white",
+        text_align="center",
+        height="auto",
+        opacity=1
+    )
+
+    style_hr = styles(
+        display="block",
+        margin=px(4, 4, "auto", "auto"),
+        border_style="inset",
+        border_width=px(0.1)
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+
+def footer():
+    myargs = [
+        #"Made in ",
+        #image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
+              #width=px(25), height=px(25)),
+        #" with ❤️ by ",
+        link("https://instagram.com/celestialbiscuit?igshid=YmMyMTA2M2Y=", "CelsetialBiscuit"),
+        #br(),
+        #link("https://buymeacoffee.com/chrischross", image('https://i.imgur.com/thJhzOO.png')),
+    ]
+    layout(*myargs)
+
+
+if __name__ == "__main__":
+    footer()
