2d118e469184a671e9bf9ceb189bd726d4503840
jahnavi2k <jahnavi2k@gmail.com>
2023-03-10 17:50:16 +0530

    changes

--- ABOUT/mainapp.py
+++ ABOUT/mainapp.py
@@ -13,25 +13,28 @@ icon_size = 20
 with col1:
     st.image("Images/poojadinew.png")
     st.caption("Pooja Gera")
-    st_button('linkedin', 'https://www.linkedin.com/in/pooja-gera/', 'LinkedIn', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/in/pooja-gera/',
+              'LinkedIn', icon_size)
 
 
 with col2:
     st.image("Images/NishthaDinew.png")
     st.caption("Nishtha Goyal")
-    st_button('linkedin', 'https://www.linkedin.com/in/nishtha0801/', 'LinkedIn', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/in/nishtha0801/',
+              'LinkedIn', icon_size)
 
 
 with col3:
     st.image("Images/GaurishaDinew.png")
     st.caption("Gaurisha R Srivastava")
-    st_button('linkedin', 'https://www.linkedin.com/in/gaurisha-r-srivastava/', 'LinkedIn', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/in/gaurisha-r-srivastava/',
+              'LinkedIn', icon_size)
 
 with col4:
     st.image("Images/AbhigyaDi.png")
     st.caption("Abhigya Verma")
-    st_button('linkedin', 'https://www.linkedin.com/in/abhigya02/', 'LinkedIn', icon_size)
-
+    st_button('linkedin', 'https://www.linkedin.com/in/abhigya02/',
+              'LinkedIn', icon_size)
 
 
 st.write("Celestial Biscuit is here to carry forward the ideology of problem solving and innovation with technology for the "
@@ -47,32 +50,33 @@ col5, col6, col7 = st.columns(3)
 with col5:
     st.image("Images/ShreyaGanjoo.jpeg")
     st.caption("Shreya Ganjoo")
-    st_button('linkedin', 'https://www.linkedin.com/in/shreya-ganjoo-30563822b/', 'LinkedIn', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/in/shreya-ganjoo-30563822b/',
+              'LinkedIn', icon_size)
 
     st.image("Images/AnushkaAggarwal.png")
     st.caption("Anushka Agarwal")
-    st_button('linkedin', 'https://www.linkedin.com/in/anushka-aggarwal-26963b228/', 'LinkedIn', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/in/anushka-aggarwal-26963b228/',
+              'LinkedIn', icon_size)
 
 
 with col6:
     st.image("Images/KashikaDi.jpg")
     st.caption("Kashika Jain")
-    st_button('linkedin', 'https://www.linkedin.com/in/kashika18/', 'LinkedIn', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/in/kashika18/',
+              'LinkedIn', icon_size)
 
     st.image("Images/KhushiJainDi.jpg")
     st.caption("Khushi Jain")
-    st_button('linkedin', 'https://www.linkedin.com/in/khushi-jain-bb3bb9201/', 'LinkedIn', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/in/khushi-jain-bb3bb9201/',
+              'LinkedIn', icon_size)
 
 with col7:
     st.image("Images/JahnaviDi.jpg")
     st.caption("Jahnavi Malhotra")
-    st_button('linkedin', 'https://www.linkedin.com/in/jahnavi2k/', 'LinkedIn', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/in/jahnavi2k/',
+              'LinkedIn', icon_size)
 
     st.image("Images/KhushiSinhaDi.jpeg")
     st.caption("Khushi Sinha")
-    st_button('linkedin', 'https://www.linkedin.com/in/khushi-sinha-91731b202/', 'LinkedIn', icon_size)
-
-
-
-
-
+    st_button('linkedin', 'https://www.linkedin.com/in/khushi-sinha-91731b202/',
+              'LinkedIn', icon_size)
